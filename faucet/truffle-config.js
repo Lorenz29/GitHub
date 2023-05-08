292097368ba88ca2c87bdb0a606046b8732b8ab1
Lorenz29 <lorenzohardoy@hotmail.com>
2023-05-08 15:33:35 -0300

    commit

--- faucet/truffle-config.js
+++ faucet/truffle-config.js
@@ -67,13 +67,13 @@ module.exports = {
     // development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
+      network_id: "*",       // Any network (default: none)
     // },
     //
     // An additional network, but with some advanced options…
     // advanced: {
     //   port: 8777,             // Custom port
-       network_id: *,       // Custom network
+    //   network_id: *,       // Custom network
     //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
     //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
     //   from: <address>,        // Account to send transactions from (default: accounts[0])
@@ -106,7 +106,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.19",      // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.6.4",      // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
